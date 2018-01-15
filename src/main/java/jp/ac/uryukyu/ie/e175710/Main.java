021027bcc86184e71f08c63e030f120eca348fba
momoka shirado <e175710@ie.u-ryukyu.ac.jp>
2018-01-15 22:22:36 +0900

    勝利判定

--- src/main/java/jp/ac/uryukyu/ie/e175710/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e175710/Main.java
@@ -7,21 +7,71 @@ public class Main {
         Deck deck = new Deck();
 
         int num = deck.draw();
+        if(num >=10){
+            num =10;
+        }
         user1.add(num);
-        System.out.println(num +"を引いた。");
+        System.out.println(user1.getName()+":"+num +"を引いた。");
 
         num = deck.draw();
+        if(num >=10){
+            num =10;
+        }
         user2.add(num);
-        System.out.println(num+"を引いた。");
+        System.out.println(user2.getName()+":"+num+"を引いた。");
 
 
         num = deck.draw();
+        System.out.println(user1.getName()+":"+num +"を引いた。");
+        if(num >=10){
+            num =10;
+        }
         user1.add(num);
-        System.out.println(num +"を引いた。"+ "合計は"+user1.getScore());
+        System.out.println("合計は"+user1.getScore());
 
         num = deck.draw();
+        System.out.println(user2.getName()+":"+num+"を引いた。");
+        if(num >=10){
+            num =10;
+        }
         user2.add(num);
-        System.out.println(num+"を引いた。"+"合計は"+user2.getScore());
+        System.out.println("合計は"+user2.getScore());
 
+        while(user1.getScore() < 16){
+            num = deck.draw();
+            System.out.println(user1.getName()+":"+num +"を引いた。");
+            if(num >=10){
+                num =10;
+            }
+            user1.add(num);
+            System.out.println("合計は"+user1.getScore());
+        }
+        while(user2.getScore() < 16){
+            num = deck.draw();
+            System.out.println(user2.getName()+":"+num+"を引いた。");
+            if(num >=10){
+                num =10;
+            }
+            user2.add(num);
+
+            System.out.println("合計は"+user2.getScore());
+        }
+        Judge(user1,user2);
+    }
+    public static void Judge(Player user1, Player user2) {
+        if(user1.getScore()<user2.getScore() &&user2.getScore()<= 21){
+            System.out.println(user2.getName() + "の勝利！");
+        }else if(user2.getScore() <user1.getScore() &&user1.getScore()<= 21){
+            System.out.println(user1.getName() + "の勝利！");
+        }
+        else if (user2.getScore()>= 21 &&user1.getScore()<= 21){
+            System.out.println(user1.getName() + "の勝利！");
+        }
+        else if (user1.getScore()>= 21 &&user2.getScore()<= 21) {
+            System.out.println(user2.getName() + "の勝利！");
+        }
+        else{
+            System.out.println("引き分け");
+        }
     }
 }
--- src/main/java/jp/ac/uryukyu/ie/e175710/Player.java
+++ src/main/java/jp/ac/uryukyu/ie/e175710/Player.java
@@ -3,16 +3,60 @@ package jp.ac.uryukyu.ie.e175710;
 import java.util.Random;
 
 public class Player{
-     private String name;//プレイヤーの名前//
-     private int score;//52枚のカード
+    private String name;//プレイヤーの名前
+    private int score;//52枚のカード
 
-     public Player(String name){
-     this.name = name;
+    public Player(String name){
+        this.name = name;
      }
-     public void add(int point){
+    public void add(int point){
        score += point;
      }
-    public String getName(){return name; }
-    public int getScore(){return score; }
+
+    public String getName(){
+         return name;
+     }
+    public int getScore(){
+        return score;
+    }
+
+
+
+/*public void judge(){
+
+        Player user1 = new Player("Taro");
+        Player user2 = new Player("Hanako");
+        Deck deck = new Deck();
+        int num = deck.draw();
+        user1.add(num);
+        System.out.println(user1.getName() + ":" + num + "を引いた。");
+
+        num = deck.draw();
+        user2.add(num);
+        System.out.println(user2.getName() + ":" + num + "を引いた。");
+
+
+        num = deck.draw();
+        user1.add(num);
+        System.out.println(user1.getName() + ":" + num + "を引いた。" + "合計は" + user1.getScore());
+
+        num = deck.draw();
+        user2.add(num);
+        System.out.println(user2.getName() + ":" + num + "を引いた。" + "合計は" + user2.getScore());
+
+        while (user1.getScore() < 16) {
+            num = deck.draw();
+            user1.add(num);
+            System.out.println(user1.getName() + ":" + num + "を引いた。" + "合計は" + user1.getScore());
+        }
+        while (user2.getScore() < 16) {
+            num = deck.draw();
+            user2.add(num);
+            System.out.println(user2.getName() + ":" + num + "を引いた。" + "合計は" + user2.getScore());
+        }
+
+
+    }*/
+
 
 }
