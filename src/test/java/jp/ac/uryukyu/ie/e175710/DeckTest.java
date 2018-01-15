c14a369ece23b5fa964df156913b32c037ca1c6c
momoka shirado <e175710@ie.u-ryukyu.ac.jp>
2018-01-16 08:27:43 +0900

    ユニットテスト

---
+++ src/test/java/jp/ac/uryukyu/ie/e175710/DeckTest.java
@@ -0,0 +1,25 @@
+package jp.ac.uryukyu.ie.e175710;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+class DeckTest {
+    int [] cards;
+    @Test
+    int draw() {
+        public int draw(){
+            Random rnd = new Random();
+            int num = 0;
+            int result ;
+            do{//0以外を引くまで続ける。
+                num = rnd.nextInt(52);
+                result = cards[num];
+            }while(result == 0);
+            cards[num] = 0;//引いたカードを０と置き換える。
+            return result;
+
+        }
+    }
+
+}
